c302518ea5038cd4b0b8fa990bb7d01e2cc26b71
lucaspontes7 <ponteslucas7@gmail.com>
2017-11-24 00:40:40 -0200

    Testes
    Testes

--- treinamento/src/main/java/com/treinamento/treinamento/rest/HelloWorldEndpoint.java
+++ treinamento/src/main/java/com/treinamento/treinamento/rest/HelloWorldEndpoint.java
@@ -1,18 +1,47 @@
 package com.treinamento.treinamento.rest;
 
-
+import com.treinamento.dao.TesteDAO;
+import com.treinamento.modelo.Teste;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-
-@Path("/helloTeste")
+@Path("/teste")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class HelloWorldEndpoint {
 
-	@GET
-	@Produces("text/plain")
-	public Response doGet() {
-		return Response.ok("Hello from WildFly Swarm!").build();
-	}
+    @GET
+    @Path("hello")
+    public Response doGet() {
+        return Response.ok("Hello from WildFly Swarm!").build();
+    }
+
+    @Resource(lookup = "jboss/datasources/ExampleDS")
+    private DataSource ds;
+
+    @GET
+    @Path("buscar")
+    public Connection dados() throws SQLException {
+       
+        Connection conn;
+       
+        conn = ds.getConnection();
+
+        
+        return conn;
+    }
+
 }
