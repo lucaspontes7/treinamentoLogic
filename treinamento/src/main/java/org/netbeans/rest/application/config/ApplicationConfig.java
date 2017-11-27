477e6bcb8266c2da52c0584eabd4232a737252ee
lucas.pontes <lucas.pontes@logicsp.com.br>
2017-11-27 17:24:17 -0200

    Teste conexão DS

--- treinamento/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ treinamento/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -30,6 +30,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(com.treinamento.treinamento.rest.HelloWorldEndpoint.class);
+        resources.add(com.treinamento.treinamento.rest.MyResource.class);
     }
     
 }
